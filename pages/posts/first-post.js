7a97ca33938e3d74d1b4b1173901deb499b7f9be
qirtudgus <qirtudgus@naver.com>
2022-05-13 21:03:30 +0900

    next에서 Link,Head,Script 태그 넣는 방식 import Link from 'next/link'; import Head from 'next/head'; import Script from 'next/script'; 으로 각각의 옵션도 가지고있기때문에 좀 더 다이나믹하게 사용 가능

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,27 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
 export default function FirstPost() {
-  return <h1>First Post</h1>;
+  return (
+    <>
+      <Head>
+        <title>First post</title>
+      </Head>
+      <Script
+        src='https://connect.facebook.net/en_US/sdk.js'
+        // strategy는 스크립트를 로드하는 시점을 제어한다.
+        strategy='lazyOnload'
+        onLoad={() => {
+          console.log('페이스북 스크립트 로드 완료');
+        }}
+      />
+      <h1>First Post</h1>
+      <h2>
+        {' '}
+        <Link href='/'>
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  );
 }
