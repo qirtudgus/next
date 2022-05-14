c453d7bc29e370eb2af85e9c54a961de494636ba
qirtudgus <qirtudgus@naver.com>
2022-05-14 10:12:40 +0900

    동적 페이지를 생성하는 방법 파일명을 []로 감싸면 동적 페이지로 인식한다. 파일명에 맞게 리턴값을 반환해주자

---
+++ pages/posts/[id].js
@@ -0,0 +1,33 @@
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+// id를 반환하는 함수를 받아온다.
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+import Layout from '../../components/layout';
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
+}
